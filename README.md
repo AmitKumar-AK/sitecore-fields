b089291e4f290e1108e00731bedcaa4b0f50c760
AmitKumar-AK <amit@abc.com>
2019-07-19 11:00:10 +0530

    Sitecore 9.x related solution changes.

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ Package installs the field definition in Core database and setups templates and 
 
 Complete installation instructions are available at:
 https://inverseproportion.wordpress.com/2017/04/12/image-field-with-cropper-in-sitecore
+https://sitecorecorner.wordpress.com/2019/06/29/advance-image-module-to-implement-focal-point-functionality-in-sitecore/
 
 Advance Image field has following dependencies:
 - Sitecore.Kernel
@@ -15,5 +16,8 @@ Advance Image field has following dependencies:
 - System.Drawing
 - System.Web
 
-# Slider
-A slider field allowing author to select value from within the defined range.
+# Updates:
+- .NET Framework 4.7.1
+- Sitecore Version: Sitecore.NET 9.1.0 (rev. 001564)
+
+
--- src/Sitecore.Fields/Extensions/HtmlExtensions.cs
+++ src/Sitecore.Fields/Extensions/HtmlExtensions.cs
@@ -1,57 +1,63 @@
 ﻿namespace Sitecore.Framework.Fields.Extensions
 {
-  using System.Web;
-  using System.Web.Mvc;
-  using System.Xml;
-  using Data.Fields;
-  using Data.Items;
-  using Mvc.Helpers;
-  using Resources.Media;
+	using System.Web;
+	using System.Web.Mvc;
+	using System.Xml;
+	using Data.Fields;
+	using Data.Items;
+	using Mvc.Helpers;
+	using Resources.Media;
 
-  public static class HtmlHelperExtensions
-  {
-    public static HtmlString AdvancedImageField(this SitecoreHelper helper, string fieldName, Item item, int height = 100, int width = 100, string cssClass = null, bool disableWebEditing = false)
-    {
-      /*
-       Example usage in view @Html.Sitecore().AdvancedImageField("Cropped", Model.Item, 100, 400)
+	public static class HtmlHelperExtensions
+	{
+		public static HtmlString AdvancedImageField(this SitecoreHelper helper, string fieldName, Item item, int height = 100, int width = 100, string cssClass = null, bool disableWebEditing = false)
+		{
+			/*
+			 Example usage in view @Html.Sitecore().AdvancedImageField("Cropped", Model.Item, 100, 400)
 
-       * */
-      if (item == null)
-        return new HtmlString("No datasource set");
+			 * */
+			if (item == null)
+				return new HtmlString("No datasource set");
 
-      var imageField = new ImageField(item.Fields[fieldName]);
+			var imageField = new ImageField(item.Fields[fieldName]);
 
-      var xml = new XmlDocument();
-      xml.LoadXml(imageField.Value);
+			if (string.IsNullOrWhiteSpace(imageField.Value))
+				return new HtmlString("Image is not provided.");
 
-      if (xml.DocumentElement == null) return new HtmlString("No cropping image parameters found.");
+			if (imageField.MediaItem == null)
+				return new HtmlString("Image Item is not found.");
 
-      var cropx = xml.DocumentElement.HasAttribute("cropx") ? xml.DocumentElement.GetAttribute("cropx") : string.Empty;
-      var cropy = xml.DocumentElement.HasAttribute("cropy") ? xml.DocumentElement.GetAttribute("cropy") : string.Empty;
-      var focusx = xml.DocumentElement.HasAttribute("focusx") ? xml.DocumentElement.GetAttribute("focusx") : string.Empty;
-      var focusy = xml.DocumentElement.HasAttribute("focusy") ? xml.DocumentElement.GetAttribute("focusy") : string.Empty;
+			var xml = new XmlDocument();
+			xml.LoadXml(imageField.Value);
 
-      float cx, cy, fx, fy;
-      float.TryParse(cropx, out cx);
-      float.TryParse(cropy, out cy);
-      float.TryParse(focusx, out fx);
-      float.TryParse(focusy, out fy);
+			if (xml.DocumentElement == null) return new HtmlString("No cropping image parameters found.");
 
-      var imageSrc = MediaManager.GetMediaUrl(imageField.MediaItem);
+			var cropx = xml.DocumentElement.HasAttribute("cropx") ? xml.DocumentElement.GetAttribute("cropx") : string.Empty;
+			var cropy = xml.DocumentElement.HasAttribute("cropy") ? xml.DocumentElement.GetAttribute("cropy") : string.Empty;
+			var focusx = xml.DocumentElement.HasAttribute("focusx") ? xml.DocumentElement.GetAttribute("focusx") : string.Empty;
+			var focusy = xml.DocumentElement.HasAttribute("focusy") ? xml.DocumentElement.GetAttribute("focusy") : string.Empty;
 
-      var src = $"{imageSrc}?cx={cx}&cy={cy}&cw={width}&ch={height}";
+			float cx, cy, fx, fy;
+			float.TryParse(cropx, out cx);
+			float.TryParse(cropy, out cy);
+			float.TryParse(focusx, out fx);
+			float.TryParse(focusy, out fy);
 
-      var hash = HashingUtils.GetAssetUrlHash(src);
+			var imageSrc = MediaManager.GetMediaUrl(imageField.MediaItem);
 
-      var builder = new TagBuilder("img");
-      builder.Attributes.Add("src", $"{src}&hash={hash}");
-      builder.Attributes.Add("alt", "");
+			var src = $"{imageSrc}?cx={cx}&cy={cy}&cw={width}&ch={height}";
 
-      if (!string.IsNullOrEmpty(cssClass))
-        builder.AddCssClass(cssClass);
+			var hash = HashingUtils.GetAssetUrlHash(src);
 
-      return MvcHtmlString.Create(builder.ToString(TagRenderMode.Normal));
+			var builder = new TagBuilder("img");
+			builder.Attributes.Add("src", $"{src}&hash={hash}");
+			builder.Attributes.Add("alt", "");
 
-    }
-  }
+			if (!string.IsNullOrEmpty(cssClass))
+				builder.AddCssClass(cssClass);
+
+			return MvcHtmlString.Create(builder.ToString(TagRenderMode.Normal));
+
+		}
+	}
 }
